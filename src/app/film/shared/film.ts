562224aa92f8740822ea27f2e72f2216d7f1df9a
Hai-Mi Nguyen <ngha1011@h-ka.de>
2022-06-23 20:42:22 +0200

    update film.ts

--- src/app/film/shared/film.ts
+++ src/app/film/shared/film.ts
@@ -6,7 +6,7 @@ export type Studio = 'WARNER' | 'DISNEY';
 
 export type FilmGenre = 'LUSTIG' | 'ACTION';
 
-export const ISBN_REGEX =
+export const ISAN_REGEX =
     // eslint-disable-next-line max-len
     /\d{3}-\d-\d{5}-\d{3}-\d|\d-\d{5}-\d{3}-\d|\d-\d{4}-\d{4}-\d|\d{3}-\d{10}|\d{13}/u;
 
@@ -14,7 +14,7 @@ export const ISBN_REGEX =
  * Model als Plain-Old-JavaScript-Object (POJO) fuer die Daten *UND*
  * Functions fuer Abfragen und Aenderungen.
  */
-export interface Buch {
+export interface Film {
     id?: string;
     version?: number;
     titel: string;
@@ -30,13 +30,13 @@ export interface Buch {
 }
 
 /**
- * Gemeinsame Datenfelder unabh&auml;ngig, ob die Buchdaten von einem Server
+ * Gemeinsame Datenfelder unabh&auml;ngig, ob die Filmdaten von einem Server
  * (z.B. RESTful Web Service) oder von einem Formular kommen.
  * Verwendung in den Interfaces:
- * - BuchServer für BuchReadService
- * - BuchForm für CreateBuchComponent
+ * - FilmServer für FilmReadService
+ * - FilmForm für CreateFilmComponent
  */
-export interface BuchShared {
+export interface FilmShared {
     titel: string | undefined;
     studio?: Studio | '';
     genre: FilmGenre;
