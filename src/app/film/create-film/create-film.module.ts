0780cc22595ac3e0f0d8cbd242efb646202e7be7
Hai-Mi Nguyen <ngha1011@h-ka.de>
2022-06-26 10:18:48 +0200

    rating geupdated

--- src/app/film/create-film/create-film.module.ts
+++ src/app/film/create-film/create-film.module.ts
@@ -1,9 +1,9 @@
 import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { CreateRating Component } from './create-rating.component';
 import { CreateFilmComponent } from './create-film.component';
 import { CreateGenreComponent } from './create-genre.component';
 import { CreateIsanComponent } from './create-isan.component';
 import { CreateNewsletterComponent } from './create-newsletter.component';
+import { CreateRatingComponent } from './create-rating.component';
 import { CreateRegisseurComponent } from './create-regisseur.component';
 import { CreateReleaseComponent } from './create-release.component';
 import { CreateSchlagwoerterComponent } from './create-schlagwoerter.component';
@@ -27,11 +27,11 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
         SharedModule,
     ],
     declarations: [
-        CreateRating Component,
         CreateFilmComponent,
         CreateGenreComponent,
         CreateIsanComponent,
         CreateNewsletterComponent,
+        CreateRatingComponent,
         CreateRegisseurComponent,
         CreateReleaseComponent,
         CreateSchlagwoerterComponent,
