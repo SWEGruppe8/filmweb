0fb217a0e6a7faca91f82c1e7c0e48af13d19ece
Dinh Thien Phan <dithienphan@gmail.de>
2022-06-29 21:59:03 +0200

    update überarbeitet

--- src/app/film/update-film/update-film.module.ts
+++ src/app/film/update-film/update-film.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { Title } from '@angular/platform-browser';
+import { UpdateBewertungComponent } from './update-bewertung.component';
 import { UpdateFilmComponent } from './update-film.component';
 import { UpdateGenreComponent } from './update-genre.component';
 import { UpdateHomepageComponent } from './update-homepage.component';
-import { UpdateRatingComponent } from './update-rating.component';
 import { UpdateStudioComponent } from './update-studio.component';
 import { UpdateTitelComponent } from './update-titel.component';
 
 @NgModule({
     imports: [SharedModule],
     declarations: [
+        UpdateBewertungComponent,
         UpdateFilmComponent,
         UpdateGenreComponent,
         UpdateHomepageComponent,
-        UpdateRatingComponent,
         UpdateStudioComponent,
         UpdateTitelComponent,
     ],
